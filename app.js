daabf45fb92f6cb5fc3b2e7e79c4d49601ed368c
SEAN CHAMPAGNE <CHAMPAGNE.SEAN@GMAIL.COM>
2016-04-12 16:16:47 -0700

    updating js functions for increased accuracy on use of console.log information

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var answer1 = prompt('Does Sean have 2 dachshunds?').toUpperCase();
 if (answer1 === 'Y' || answer1 === 'YES') {
   alert('BOOM!  They\'re freaking adorable.');
 } else alert('WRONG!  They\'re freaking adorable.');
-console.log('Sean has 2 dachshunds named Hansel and Wimbledon.');
+console.log('User Response: ' + answer1 + ' Correct Response:  YES');
 
 var answer2 = prompt('Is Sean married?').toUpperCase();
 
@@ -16,7 +16,7 @@ if (answer2 === 'Y' || answer2 === 'YES')
 {
   alert('Correct, ' + userName + '!  He is!');
 } else alert('WRONG AGAIN, ' + userName + '.  You know nothing,' + userName + '.');
-console.log('Sean is married to Bode.');
+console.log('User Response: ' + answer2 + ' Correct Response:  YES');
 
 var answer3 = prompt('Is Sean on his way to becoming a master coder?').toUpperCase();
 
@@ -24,7 +24,7 @@ if (answer3 === 'Y' || answer3 === 'YES')
 {
   alert('That is the right attitude!');
 } else alert('That is the WRONG attitude.');
-console.log('Sean is a future master coder.');
+console.log('User Response: ' + answer3 + ' Correct Response:  YES');
 
 var answer4 = prompt('Does Sean live in Seattle?').toUpperCase();
 
@@ -32,7 +32,7 @@ if (answer4 === 'Y' || answer4 === 'YES')
 {
   alert('Correct, ' + userName + '.  He lives on Queen Anne.');
 } else alert('Incorrect, ' + userName + '.  He lives on Queen Anne.');
-console.log('Sean lives in Seattle, Washington.');
+console.log('User Response: ' + answer4 + ' Correct Response:  YES');
 
 var answer5 = prompt('Is \'Champagne\' really his last name?').toUpperCase();
 
@@ -40,4 +40,4 @@ if (answer5 === 'Y' || answer5 === 'YES')
 {
   alert('Hahahah yes.  People ask him all the time.');
 } else ('Incorrect.  It is true.  Champagne is his family name.');
-console.log('Sean Champagne is the real Sean Champagne.');
+console.log('User Response: ' + answer5 + ' Correct Response:  YES');
